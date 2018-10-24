a5e1f2460719a1730d441f074cb58830e97f12b1
Hrvoje Bušić <hrvoje@Hrvojes-Macbook-Pro.local>
2018-10-24 22:01:55 +0200

    [ADD] Task 3 done.

--- lab1/src/main/java/hr/fer/rgkk/transactions/MultiSigTransaction.java
+++ lab1/src/main/java/hr/fer/rgkk/transactions/MultiSigTransaction.java
@@ -1,8 +1,13 @@
 package hr.fer.rgkk.transactions;
 
+import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Transaction;
+import org.bitcoinj.crypto.TransactionSignature;
 import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
+
+import static org.bitcoinj.script.ScriptOpCodes.*;
 
 /**
  * You must implement locking and unlocking script such that transaction output is locked by one mandatory authority
@@ -10,20 +15,50 @@ import org.bitcoinj.script.Script;
  */
 public class MultiSigTransaction extends ScriptTransaction {
 
+    private ECKey bank;
+    private ECKey fst;
+    private ECKey snd;
+    private ECKey trd;
+
     public MultiSigTransaction(WalletKit walletKit, NetworkParameters parameters) {
         super(walletKit, parameters);
+        bank = getWallet().freshReceiveKey();
+        fst = getWallet().freshReceiveKey();
+        snd = getWallet().freshReceiveKey();
+        trd = getWallet().freshReceiveKey();
     }
 
     @Override
     public Script createLockingScript() {
         // TODO: Create Locking script
-        throw new UnsupportedOperationException();
+        return new ScriptBuilder()
+                .number(1)
+                .data(bank.getPubKey())
+                .number(1)
+                .op(OP_CHECKMULTISIGVERIFY)
+                .number(1)
+                .data(fst.getPubKey())
+                .data(snd.getPubKey())
+                .data(trd.getPubKey())
+                .number(3)
+                .op(OP_CHECKMULTISIG)
+                .build();
     }
 
     @Override
     public Script createUnlockingScript(Transaction unsignedTransaction) {
         // TODO: Please be aware of the OP_CHECK_MULTISIG bug!
         // TODO: Create Unlocking script
-        throw new UnsupportedOperationException();
+        TransactionSignature s_b = sign(unsignedTransaction, bank);
+        // Uncomment 1 signature here
+        TransactionSignature s_c = sign(unsignedTransaction, fst);
+        //TransactionSignature s_c = sign(unsignedTransaction, snd);
+        //TransactionSignature s_c = sign(unsignedTransaction, trd);
+        return new ScriptBuilder()
+                .number(42)
+                .data(s_c.encodeToBitcoin())
+                .number(42)
+                .data(s_b.encodeToBitcoin())
+                .build();
     }
 }
