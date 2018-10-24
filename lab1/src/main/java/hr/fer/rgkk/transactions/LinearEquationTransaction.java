b7900912210a065f65944ef09e32f8852859491d
Hrvoje Bušić <hrvoje@Hrvojes-Macbook-Pro.local>
2018-10-24 21:17:43 +0200

    [ADD] Task 2 done.

--- lab1/src/main/java/hr/fer/rgkk/transactions/LinearEquationTransaction.java
+++ lab1/src/main/java/hr/fer/rgkk/transactions/LinearEquationTransaction.java
@@ -3,6 +3,9 @@ package hr.fer.rgkk.transactions;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Transaction;
 import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
+
+import static org.bitcoinj.script.ScriptOpCodes.*;
 
 /**
  * You must implement locking and unlocking script such that transaction output is locked by 2 integers x and y
@@ -16,19 +19,44 @@ import org.bitcoinj.script.Script;
  */
 public class LinearEquationTransaction extends ScriptTransaction {
 
+    private Integer x=-1424;
+    private Integer y=1460;
+    private Integer fst=36;
+    private Integer lst=2884;
+
     public LinearEquationTransaction(WalletKit walletKit, NetworkParameters parameters) {
         super(walletKit, parameters);
+    }
+
+    private byte[] intToByteArray(int value) {
+        return new byte[] {
+                (byte)(value >>> 24),
+                (byte)(value >>> 16),
+                (byte)(value >>> 8),
+                (byte)value};
     }
 
     @Override
     public Script createLockingScript() {
         // TODO: Create Locking script
-        throw new UnsupportedOperationException();
+        return new ScriptBuilder()
+                .op(OP_2DUP)
+                .op(OP_ADD)
+                .number(fst)
+                .op(OP_EQUALVERIFY)
+                .op(OP_SUB)
+                .op(OP_ABS)
+                .number(lst)
+                .op(OP_EQUAL)
+                .build();
     }
 
     @Override
     public Script createUnlockingScript(Transaction unsignedScript) {
         // TODO: Create Unlocking script
-        throw new UnsupportedOperationException();
+        return new ScriptBuilder()
+                .number(x)
+                .number(y)
+                .build();
     }
 }
